ff7e64576de0d6f5b95fb5fd21079ddd1401a6a6
Dani <79227896+Dani780-C@users.noreply.github.com>
2022-06-15 22:47:08 +0300

    Add files via upload

--- New folder/Assets/Scripts/Menu/MainMenu.cs
+++ New folder/Assets/Scripts/Menu/MainMenu.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
-    public void PlayGame(){
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    public void LevelOne(){
+        SceneManager.LoadScene(1);
     }
+
+    public void LevelTwo(){
+        SceneManager.LoadScene(2);
+    }
+
     public void QuitGame(){
         Debug.Log("QUIT!");
         Application.Quit();
