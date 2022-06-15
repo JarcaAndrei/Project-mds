c3da2d2f75d98914837bdfc78343460de24ba4ad
Dani <79227896+Dani780-C@users.noreply.github.com>
2022-06-15 22:46:10 +0300

    Add files via upload

--- New folder/Assets/Scripts/Health/Health.cs
+++ New folder/Assets/Scripts/Health/Health.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Health : MonoBehaviour
 {
@@ -54,6 +55,7 @@ public class Health : MonoBehaviour
 
                 dead = true;
                 SoundManager.instance.PlaySound(deathSound);
+
             }
         }
     }
